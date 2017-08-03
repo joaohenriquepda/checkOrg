f2394140d3a7a264f05bdc50d4e9bb9d18e07409
João Henrique <joaohenrique.p.almeida@gmail.com>
2017-08-02 22:20:33 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OrganizationComponent } from './organization/organization.component'
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+  data:any = null;
+
+  constructor(private _http:Http){
+      console.log("Ola");
+      this.getInfo();
+    }  
+
+  private getInfo(){
+    console.log("!");
+    
+    return this._http.get('https://conduit.productionready.io/api/profiles/eric').map((res:Response) => res.json()).subscribe(data => {
+                        this.data = data;
+                        console.log(this.data);
+                });
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OrganizationComponent } from './organization/organization.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationComponent
   ],
   imports: [
+    HttpModule,
     BrowserModule
   ],
   providers: [],
