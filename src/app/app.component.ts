957405dffaf9c8a7be6497f48c1196306be28562
João Henrique <joaohenrique.p.almeida@gmail.com>
2017-08-03 00:31:23 -0300

    Add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
 import { Component } from '@angular/core';
-import { OrganizationComponent } from './organization/organization.component'
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { OrganizationComponent } from './organization/organization.component';
+import { GithubService } from './services/github.service';
+
 
 @Component({
   selector: 'app-root',
+  providers:[GithubService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
 
   title = 'app';
-  data:any = null;
 
-  constructor(private _http:Http){
-      console.log("Ola");
-      this.getInfo();
-    }  
+  constructor(private github: GithubService ){
 
-  private getInfo(){
-    console.log("!");
-    
-    return this._http.get('https://conduit.productionready.io/api/profiles/eric').map((res:Response) => res.json()).subscribe(data => {
-                        this.data = data;
-                        console.log(this.data);
-                });
+  }
+
+  ngOnInit(){
+    this.getInfor();
   }
 
 
+  private getInfor(){
+    this.github.getInfo();
+  }
 
+  
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class GithubService {
 
-  constructor() { }
+  data:any = null;
+
+  constructor(private _http:Http){
+    
+    }  
+
+  public getInfo(){
+    console.log("!");
+        return this._http.get('https://conduit.productionready.io/api/profiles/eric').map((res:Response) => res.json()).subscribe(data => {
+                        this.data = data;
+                        console.log(this.data);
+                });
+  }
 
 }
